649de2803f919d6efa9f36e42454b36a882f94b0
Juan Camilo Guzman <jcamiloguz1@gmail.com>
2020-07-01 21:37:19 -0500

    Exam complete

---
+++ examn.js
@@ -0,0 +1,10 @@
+var nombre = 'Sacha'
+var apellido = 'Lifszyc'
+
+function pasarAMayusculas (nombre) {
+    return console.log(nombre.toUpperCase())
+}
+
+pasarAMayusculas(nombre + ' ' + apellido)
+pasarAMayusculas('Lucía')
+pasarAMayusculas('')
---
+++ examn2.js
@@ -0,0 +1,4 @@
+obtenerPersonaje('https://swapi.co/api/people/1')
+        .then(luke => console.log(luke.name))
+
+      console.log('Sacha')
